864c1fcb3dcf18a7db97bf284275009a971adaa8
Leif Lundgren (Mitel) <leif.lundgren@mitel.com>
2024-10-12 13:53:44 +0200

    Read from double water sensors

--- thermometer.py
+++ thermometer.py
@@ -26,23 +26,31 @@ def read_temp_raw( adr ):
 def read_temp_raw_subprocess( adr ):
         devfile = os.path.join(base_dir, adr, 'w1_slave')
         print 'Readin from ' + devfile
-        with open(devfile, "r") as fil:
-          lines = fil.readlines()
-          print 'raw data ' + adr
-          print lines
-          return lines
-
+        try:
+            with open(devfile, "r") as fil:
+                lines = fil.readlines()
+                print 'raw data ' + adr
+                print lines
+                return lines
+        except IOError, e:
+            print "Read fail", e
+            return "read fail"
 
 def read_temp( adr ):
     lines = read_temp_raw_subprocess( adr )
-    if lines[0].strip()[-3:] != 'YES':
-      print 'CRC failed'
-      return "Vem vet?"
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        return str(round(temp_c, 1)) + " C"
+    try:
+        if lines[0].strip()[-3:] != 'YES':
+            print 'CRC failed'
+            return "Vem vet?"
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            return str(round(temp_c, 1)) + " C"
+        else:
+            return "to temp found"
+    except:
+        return "parse error"
 
 def format_lcd(s):
   return s.ljust(20, ' ')
@@ -53,7 +61,8 @@ time.sleep(1)
 while True:
         lcd.lcd_display_string(format_lcd("Bastu   " + read_temp('28-0301a279a212')), 1)
         lcd.lcd_display_string(format_lcd("Ute     " + read_temp('28-000004374902')), 2)
-        lcd.lcd_display_string(format_lcd("Vatten  " + read_temp('28-0000045d8f34')), 3)
+        lcd.lcd_display_string(format_lcd("Vatten1 " + read_temp('28-e9d37d1f64ff')), 3)
+        lcd.lcd_display_string(format_lcd("Vatten2 " + read_temp('28-f2d77d1f64ff')), 4)
         now = datetime.datetime.now()
 #       lcd.lcd_display_string(now.strftime("%Y-%m-%d %H:%M:%S"), 4)
         time.sleep(30)
